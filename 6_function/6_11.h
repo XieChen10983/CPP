cb41f2feed119257262ca9c2f64b8132955a1e63
XieChen10983 <928978561@qq.com>
2021-02-02 15:54:20 +0800

    函数的引用形参

---
+++ 6_function/6_11.h
@@ -0,0 +1,16 @@
+//
+// Created by Administrator on 2021/2/2.
+//
+
+#ifndef CHANNING_6_11_H
+#define CHANNING_6_11_H
+
+/*
+ * 6.11 编写并验证自己的reset函数，使其作用于引用类型的参数
+ */
+
+void resetInt(int& a){
+    a = 0;
+}
+
+#endif //CHANNING_6_11_H
--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 #include "6_function/6_5.h"
 #include "6_function/6_10.h"
 #include "6_function/6_12.h"
-#include "6_function/6_7.h"
+#include "6_function/6_11.h"
 using namespace std;
 
 void test(){
-    for (int i = 0; i < 10; i++)
-        cout << countUseTime() << endl;
+    int b = 50;
+    resetInt(b);
+    cout << b << endl;
 }
 
 int main() {

6b7a84ffd0bf257ea7cb439e08370741db56c7ea
XieChen10983 <928978561@qq.com>
2021-02-04 14:04:33 +0800

    C++的顺序容器概述

--- main.cpp
+++ main.cpp
@@ -1,38 +1,11 @@
 #include <iostream>
 #include "6_function/6_3.h"
-#include "6_function/6_4.h"
-#include "6_function/6_5.h"
-#include "6_function/6_10.h"
-#include "6_function/6_12.h"
-#include "6_function/6_44.h"
-#include "6_function/6_54.h"
 #include <string>
 #include <vector>
 using namespace std;
 
-int add_(int a, int b){
-    return a + b;
-}
-
-int sub(int a, int b){
-    return a - b;
-}
-
-void mul(int a, int b){
-    cout << a * b << endl;
-}
-
 void test() {
-//    cout << isShorter("jlsk", "lskjgsj") << endl;
-    v.push_back(add_);
-    v.push_back(&sub);
-//    v.push_back(mul);
-    cout << v.size() << endl;
 
-    int a = 5, b = 3;
-    for (auto & i : v){
-        cout << i(a, b) << endl;
-    }
 }
 
 int main() {

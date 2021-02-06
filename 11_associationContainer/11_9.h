72cb85984267a6a09219143a9e7db53f085ff67b
XieChen10983 <928978561@qq.com>
2021-02-06 13:36:14 +0800

    map和set的关键字类型的要求

---
+++ 11_associationContainer/11_9.h
@@ -0,0 +1,13 @@
+//
+// Created by Administrator on 2021/2/6.
+//
+
+#ifndef CHANNING_11_9_H
+#define CHANNING_11_9_H
+#include <map>
+#include <list>
+#include <string>
+using namespace std;
+
+map<string, list<int>> m;
+#endif //CHANNING_11_9_H
